af8074d18c0c2d3775b1cad3470f259d8bbcb7d9
Shirley <moralesshirley218@gmail.com>
2024-09-23 16:55:40 -0500

    Creacion de la funcion put

--- main.py
+++ main.py
@@ -40,3 +40,33 @@ def add_contact(new_contact: ContactModel):
         "success" : True,
         "message" : "Added new Contact"
     }
+    
+@app.put("/api/contacts/{id_contact}")
+def update_contact(id_contact: str, new_contact: ContactModel):
+    contacts = md.read_contacts()
+    
+    for index, contact in enumerate(contacts):
+        if str(contact["id"]) == id_contact:  
+            updated_contact = new_contact.dict()
+
+            
+            if new_contact.name == "":
+                updated_contact["name"] = contact["name"]
+                
+            if new_contact.phone == "":
+                updated_contact["phone"] = contact["phone"]
+
+           
+            contacts[index] = updated_contact
+            
+            
+            md.write_contacts(contacts)
+            
+            return {
+                "success": True,
+                "message": "Contact updated successfully"
+            }
+    
+    raise HTTPException(status_code=404, detail="Contact not found")
+
+
