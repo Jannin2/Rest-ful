9bbf57ff8052899ad62fdcbea14ba63cb9318282
Shirley <moralesshirley218@gmail.com>
2024-09-23 17:12:28 -0500

    creada la funcion de delete

--- main.py
+++ main.py
@@ -69,4 +69,21 @@ def update_contact(id_contact: str, new_contact: ContactModel):
     
     raise HTTPException(status_code=404, detail="Contact not found")
 
+@app.delete("/api/contacts/{id_contact}")
+def remove_contact(id_contact:str):
+    contacts = md.read_contacts()
+    
+    for index, contact in enumerate(contacts):
+        if contact["id"] == id_contact:
+            contacts.pop(index)
+            
+            md.write_contacts(contacts)
+            
+            return {
+                "succes" : True,
+                "message" : "Deleted contact"
+            }
+            
+    raise HTTPException(status_code= 404, detail= "contact not found")
+
 
